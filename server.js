740aeb4ca17fc62336ff03c84ef9c68932df268d
Ben O'Leary <Bolearyecdl@gmail.com>
2020-04-28 16:59:05 +0000

    added mongodb files to ca1 files

---
+++ server.js
@@ -0,0 +1,27 @@
+var logger = require("morgan"),
+cors = require("cors"),
+http = require("http"),
+express = require("express"),
+bodyParser = require("body-parser"),
+mongoose = require('mongoose');
+require=('dotenv').config();
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(require('./routes'));
+
+app.listen(port, function(err){
+    console.log("Listening on Port: " + port)
+});
+
+mongoose.connect(process.env.MONGODB_URL);
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
+});
